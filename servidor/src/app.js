89751a8fc12e58a02b696383c0450478082c5e1c
diegoavilap <diegoavilapava@hotmail.com>
2019-04-08 17:28:24 -0500

    Modulo Bootstrap

--- servidor/src/app.js
+++ servidor/src/app.js
@@ -9,6 +9,15 @@ require('./helpers')
 const directorioPublico = path.join(__dirname, '../public');
 const directorioPartials = path.join(__dirname, '../partials');
 
+const dirNode_modules = path.join(__dirname, '../node_modules')
+
+app.use('/css', express.static(dirNode_modules + '/bootstrap/dist/css'));
+app.use('/js', express.static(dirNode_modules + '/jquery/dist'));
+app.use('/js', express.static(dirNode_modules + '/popper.js/dist'));
+
+app.use('/js', express.static(dirNode_modules + '/bootstrap/dist/js'));
+
+
 app.use(express.static(directorioPublico));
 
 hbs.registerPartials(directorioPartials);
@@ -29,7 +38,9 @@ app.post('/calculos', (req, res) =>{
         notas3: parseInt(req.body.nota3)
     });
 });
-
+app.get('/listado', (req, res) =>{
+    res.render('listado');
+});
 app.get('*', (req, res) =>{
     res.render('error', {
         estudiante: 'error'
--- servidor/src/helpers.js
+++ servidor/src/helpers.js
@@ -6,8 +6,8 @@ hbs.registerHelper('obtenerPromedio', (nota1, nota2, nota3) =>{
 
 hbs.registerHelper('listar', () =>{
     listaEst = require('./listado.json');
-    let texto = "<table>" +
-                "<thead>"+
+    let texto = "<table class='table table-striped table-hover'>" +
+                "<thead class='thead-dark'>"+
                 "<th>Nombre </th>"+ 
                 "<th>Matematicas </th>"+ 
                 "<th>Ingles </th>"+
@@ -25,5 +25,34 @@ hbs.registerHelper('listar', () =>{
     });
     texto += '</tbody>' +
              '</table>';
+    return texto;
+});
+
+
+hbs.registerHelper('listar2', () =>{
+    listaEst = require('./listado.json');
+    let texto = '<div class="accordion" id="accordionExample">';
+    i = 1;
+    listaEst.forEach(estudiante => {
+        texto += `<div class="card">
+                    <div class="card-header" id="heading${i}">
+                    <h2 class="mb-0">
+                        <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapse${i}" aria-expanded="true" aria-controls="collapseOne">
+                            ${estudiante.nombre}
+                        </button>
+                    </h2>
+                    </div>
+
+                    <div id="collapse${i}" class="collapse" aria-labelledby="heading${i}" data-parent="#accordionExample">
+                        <div class="card-body">
+                            Tiene una nota en matematicas de ${estudiante.matematicas} <br/>
+                            Tiene una nota en ingles de ${estudiante.ingles} <br/>
+                            Tiene una nota en programacion de ${estudiante.programacion} <br/>
+                        </div>
+                    </div>
+                </div>`;
+                i++;
+    });
+    texto += '</div>';
     return texto;
 });
